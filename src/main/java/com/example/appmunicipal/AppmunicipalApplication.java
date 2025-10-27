e6cb98852489be7d0e080a5aba1f969959b73f8e
jesus-acev <j.acevedo03@ufromail.cl>
2025-10-27 09:48:29 -0300

    add: Se agrega SpringBoot con maven

---
+++ src/main/java/com/example/appmunicipal/AppmunicipalApplication.java
@@ -0,0 +1,13 @@
+package com.example.appmunicipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmunicipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmunicipalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appmunicipal/AppmunicipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appmunicipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmunicipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
